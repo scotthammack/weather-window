a201d24ca3cdc3902681447cfd7530a136d698ef
scotthammack <shammack@gmail.com>
2017-06-21 15:59:49 +0000

    Store the delta value instead of recalculating it. Added a string to include in the subject of the emails for easy filtering.

--- weather-window.py
+++ weather-window.py
@@ -15,6 +15,7 @@ password = os.environ['WINDOW_EMAIL_PASSWORD']
 primed = None
 last_temp = None
 threshold = 72
+subject_tag = "[weather-window] "
 
 url = 'http://api.wunderground.com/api/%s/conditions/forecast/q/%s.json' % ( wunderground_key, long_lat )
 
@@ -49,6 +50,7 @@ while True:
     if not last_temp:
         last_temp = current_temp
         mesg = "Current temperature is %1.1f." % current_temp
+    delta = current_temp - last_temp
 
     timestamp = time.strftime('%H:%M: ', current_time)
 
@@ -56,32 +58,32 @@ while True:
         if ( current_temp > threshold ):
             print "It's too hot to have the window open."
             primed = True
-            send_mail(mesg, email_address, "It's hot")
+            send_mail(mesg, email_address, subject_tag + "It's hot")
         else:
             print "It's safe to have the window open."
             primed = False
-            send_mail(mesg, email_address, "It's not that hot")
+            send_mail(mesg, email_address, subject_tag + "It's not that hot")
     elif ( primed and ( current_temp > threshold ) ):
         mesg = "Temperature dropped to %1.1f. Open the window!" % current_temp
         primed = False
-        send_mail(mesg, email_address, 'Time to open the window!')
+        send_mail(mesg, email_address, subject_tag + 'Time to open the window!')
     elif ( not primed and ( current_temp > threshold ) ):
         mesg = "Temperature rose to %1.1f. Close the window!" % current_temp
         primed = True
-        send_mail(mesg, email_address, 'Time to close the window!')
-    elif ( current_temp - last_temp > 0.1 ):
+        send_mail(mesg, email_address, subject_tag + 'Time to close the window!')
+    elif ( delta > 0.1 ):
         if not was_rising:
-            mesg = "WARNING! Temperature has started rising! Currently %1.1f." % current_temp
-            send_mail(mesg, email_address, 'Heat rising!')
+            mesg = "WARNING! Temperature has started rising! Currently %1.1f (an increase of %1.1f)." % (current_temp, delta)
+            send_mail(mesg, email_address, subject_tag + 'Heat rising!')
         else:
-            mesg = "Temperature rose to %1.1f." % current_temp
+            mesg = "Temperature rose by %1.1f to %1.1f." % (delta, current_temp)
         was_rising = True
-    elif ( current_temp - last_temp < -0.1 ):
+    elif ( delta < -0.1 ):
         if was_rising:
-            mesg = "Relief is in sight... the temperature has started dropping. Currently %1.1f." % current_temp
-            send_mail(mesg, email_address, 'Good news!')
+            mesg = "Relief is in sight... the temperature has started dropping. Currently %1.1f (a decrease of %1.1f." % (current_temp, delta)
+            send_mail(mesg, email_address, subject_tag + 'Good news!')
         else:
-            mesg = "Temperature dropped to %1.1f." % current_temp
+            mesg = "Temperature dropped by %1.1 to %1.1f." % (delta, current_temp)
         was_rising = False
     else:
         be_quiet = True
