00b5890799c5551f17c74e7dd0ecb2c77a4d18f2
Scott Hammack <scott@hammack.exposed>
2017-06-22 08:30:50 -0700

    capitalized constant names and reorganized slightly

--- weather-window.py
+++ weather-window.py
@@ -6,40 +6,38 @@ import time
 
 from email.mime.text import MIMEText
 
-api_key = os.environ['WUNDERGROUND_API_KEY']
-wunderground_key = os.environ['WUNDERGROUND_KEY']
-long_lat = os.environ['LONG_LAT']
-email_address = os.environ['WINDOW_EMAIL_ADDRESS']
-password = os.environ['WINDOW_EMAIL_PASSWORD']
-mail_server = 'smtp.gmail.com:587'
+API_KEY = os.environ['WUNDERGROUND_API_KEY']
+WUNDERGROUND_KEY = os.environ['WUNDERGROUND_KEY']
+LONG_LAT = os.environ['LONG_LAT']
+EMAIL_ADDRESS = os.environ['WINDOW_EMAIL_ADDRESS']
+PASSWORD = os.environ['WINDOW_EMAIL_PASSWORD']
+MAIL_SERVER = 'smtp.gmail.com:587'
 
-threshold = 72
-delta_threshold = 0.1
-subject_tag = "[weather-window] "
-noisy_emails = False
+THRESHOLD = 72
+DELTA_THRESHOLD = 0.1
+SUBJECT_TAG = "[weather-window] "
+NOISY_EMAILS = False
+URL = 'http://api.wunderground.com/api/%s/conditions/forecast/q/%s.json' % ( WUNDERGROUND_KEY, LONG_LAT )
 
 primed = None
 last_temp = None
-
-url = 'http://api.wunderground.com/api/%s/conditions/forecast/q/%s.json' % ( wunderground_key, long_lat )
+was_rising = True
 
 def send_mail(mesg, recipient, subj):
 	mesg = MIMEText(mesg)
 	mesg['Subject'] = subj
 	mesg['To'] = recipient
 
-	s = smtplib.SMTP(mail_server)
+	s = smtplib.SMTP(MAIL_SERVER)
 	s.starttls()
-	s.login(email_address, password)
-	s.sendmail(email_address, recipient, mesg.as_string())
+	s.login(EMAIL_ADDRESS, PASSWORD)
+	s.sendmail(EMAIL_ADDRESS, recipient, mesg.as_string())
 	s.quit()
-
-was_rising = True
 
 while True:
 	be_quiet = False
 
-	r = requests.get(url)
+	r = requests.get(URL)
 
 	if r.status_code != 200:
 		print "Bad status code: "
@@ -59,37 +57,37 @@ while True:
 	timestamp = time.strftime('%H:%M: ', current_time)
 
 	if ( primed == None ):
-		if ( current_temp > threshold ):
+		if ( current_temp > THRESHOLD ):
 			print "It's too hot to have the window open."
 			primed = True
-			if noisy_emails:
-				send_mail(mesg, email_address, subject_tag + "It's hot")
+			if NOISY_EMAILS:
+				send_mail(mesg, EMAIL_ADDRESS, SUBJECT_TAG + "It's hot")
 		else:
 			print "It's safe to have the window open."
 			primed = False
-			if noisy_emails:
-				send_mail(mesg, email_address, subject_tag + "It's not that hot")
-	elif ( primed and ( current_temp < threshold ) ):
+			if NOISY_EMAILS:
+				send_mail(mesg, EMAIL_ADDRESS, SUBJECT_TAG + "It's not that hot")
+	elif ( primed and ( current_temp < THRESHOLD ) ):
 		mesg = "Temperature dropped to %1.1f. Open the window!" % current_temp
 		primed = False
-		send_mail(mesg, email_address, subject_tag + 'Time to open the window!')
-	elif ( not primed and ( current_temp > threshold ) ):
+		send_mail(mesg, EMAIL_ADDRESS, SUBJECT_TAG + 'Time to open the window!')
+	elif ( not primed and ( current_temp > THRESHOLD ) ):
 		mesg = "Temperature rose to %1.1f. Close the window!" % current_temp
 		primed = True
-		send_mail(mesg, email_address, subject_tag + 'Time to close the window!')
-	elif ( delta > delta_threshold ):
+		send_mail(mesg, EMAIL_ADDRESS, SUBJECT_TAG + 'Time to close the window!')
+	elif ( delta > DELTA_THRESHOLD ):
 		if not was_rising:
 			mesg = "WARNING! Temperature has started rising! Currently %1.1f (an increase of %1.1f)." % (current_temp, delta)
-			if noisy_emails:
-				send_mail(mesg, email_address, subject_tag + 'Heat rising!')
+			if NOISY_EMAILS:
+				send_mail(mesg, EMAIL_ADDRESS, SUBJECT_TAG + 'Heat rising!')
 		else:
 			mesg = "Temperature rose by %1.1f to %1.1f." % (delta, current_temp)
 		was_rising = True
-	elif ( delta < (delta_threshold * -1) ):
+	elif ( delta < (DELTA_THRESHOLD * -1) ):
 		if was_rising:
 			mesg = "Relief is in sight... the temperature has started dropping. Currently %1.1f (a decrease of %1.1f)." % (current_temp, delta * -1)
-			if noisy_emails:
-				send_mail(mesg, email_address, subject_tag + 'Good news!')
+			if NOISY_EMAILS:
+				send_mail(mesg, EMAIL_ADDRESS, SUBJECT_TAG + 'Good news!')
 		else:
 			mesg = "Temperature dropped by %1.1f to %1.1f." % (delta * -1, current_temp)
 		was_rising = False
